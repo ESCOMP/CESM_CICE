97b5f5ac2f7a45d49a50ef3a984c12f2399050e5
dbailey <dbailey@fe37f545-8307-0410-aea5-b40df96820b5>
2007-08-20 21:53:04 +0000

    Fix npt bug
    git-svn-id: https://svn-ccsm-models.cgd.ucar.edu/cice/trunk@6107 fe37f545-8307-0410-aea5-b40df96820b5

--- src/source/ice_calendar.F90
+++ src/source/ice_calendar.F90
@@ -208,7 +208,9 @@
 
       idate = (nyr+year_init-1)*10000 + month*100 + mday ! date (yyyymmdd) 
 
+#ifndef CCSM
       if (istep >= npt+1)  stop_now = 1
+#endif
       if (nyr   /= nyrp)   new_year = .true.
       if (month /= monthp) new_month = .true.
       if (mday  /= mdayp)  new_day = .true.
