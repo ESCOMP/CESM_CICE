b7649938e314cb01542e20cd0374cad85ec1e224
dbailey <dbailey@fe37f545-8307-0410-aea5-b40df96820b5>
2009-12-28 22:16:35 +0000

    Updated documentation.
    git-svn-id: https://svn-ccsm-models.cgd.ucar.edu/cice/trunk@20089 fe37f545-8307-0410-aea5-b40df96820b5

--- docs/UsersGuide/ice_usrdoc/internals.pl
+++ docs/UsersGuide/ice_usrdoc/internals.pl
@@ -3,7 +3,7 @@
 
 
 $key = q/cite_hunk01/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:ice_prescribed_nml/;
@@ -27,11 +27,11 @@ $ref_files{$key} = "$dir".q|node10.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/ncat/;
-$ref_files{$key} = "$dir".q|node21.html|; 
+$ref_files{$key} = "$dir".q|node22.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/stdout/;
-$ref_files{$key} = "$dir".q|node23.html|; 
+$ref_files{$key} = "$dir".q|node24.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/setup_nml_mgmt/;
@@ -39,7 +39,7 @@ $ref_files{$key} = "$dir".q|node9.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/history/;
-$ref_files{$key} = "$dir".q|node26.html|; 
+$ref_files{$key} = "$dir".q|node27.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/uncoupled_scripts/;
@@ -47,31 +47,31 @@ $ref_files{$key} = "$dir".q|node5.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/pointer_files/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$ref_files{$key} = "$dir".q|node26.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/runtypes/;
-$ref_files{$key} = "$dir".q|node20.html|; 
+$ref_files{$key} = "$dir".q|node21.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:setup_nml/;
 $ref_files{$key} = "$dir".q|node8.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_thor75/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lipshunke04/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/table:nml_write/;
 $ref_files{$key} = "$dir".q|node11.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_thor75/;
-$ref_files{$key} = "$dir".q|node37.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lipshunke04/;
-$ref_files{$key} = "$dir".q|node37.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/example2_nml/;
-$ref_files{$key} = "$dir".q|node18.html|; 
+$ref_files{$key} = "$dir".q|node19.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/coupled_scripts/;
@@ -83,7 +83,7 @@ $ref_files{$key} = "$dir".q|node9.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/cite_lips01/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/nml_physics/;
@@ -91,19 +91,19 @@ $ref_files{$key} = "$dir".q|node12.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/table:history_fields/;
-$ref_files{$key} = "$dir".q|node29.html|; 
+$ref_files{$key} = "$dir".q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_hunk97/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_cice08/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_Briegleb07/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:ice_prescraero_nml/;
@@ -111,11 +111,15 @@ $ref_files{$key} = "$dir".q|node14.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/history_files/;
-$ref_files{$key} = "$dir".q|node26.html|; 
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:domain_nml/;
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_bitz01/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:nml_time/;
@@ -127,7 +131,7 @@ $ref_files{$key} = "$dir".q|node13.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/cite_bitz99/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:nml_ocn/;
@@ -135,19 +139,19 @@ $ref_files{$key} = "$dir".q|node14.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/change_content/;
-$ref_files{$key} = "$dir".q|node29.html|; 
+$ref_files{$key} = "$dir".q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/restart_files/;
-$ref_files{$key} = "$dir".q|node24.html|; 
+$ref_files{$key} = "$dir".q|node25.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_roth75b/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/avg_fields/;
-$ref_files{$key} = "$dir".q|node27.html|; 
+$ref_files{$key} = "$dir".q|node28.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/example5_nml/;
@@ -163,15 +167,15 @@ $ref_files{$key} = "$dir".q|node8.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/example1_nml/;
-$ref_files{$key} = "$dir".q|node17.html|; 
+$ref_files{$key} = "$dir".q|node18.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:time_grid_hist/;
-$ref_files{$key} = "$dir".q|node29.html|; 
+$ref_files{$key} = "$dir".q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_hibl79/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- docs/UsersGuide/ice_usrdoc/labels.pl
+++ docs/UsersGuide/ice_usrdoc/labels.pl
@@ -3,7 +3,7 @@
 
 
 $key = q/cite_hunk01/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:ice_prescribed_nml/;
@@ -27,11 +27,11 @@ $external_labels{$key} = "$URL/" . q|node10.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/ncat/;
-$external_labels{$key} = "$URL/" . q|node21.html|; 
+$external_labels{$key} = "$URL/" . q|node22.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/stdout/;
-$external_labels{$key} = "$URL/" . q|node23.html|; 
+$external_labels{$key} = "$URL/" . q|node24.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/setup_nml_mgmt/;
@@ -39,7 +39,7 @@ $external_labels{$key} = "$URL/" . q|node9.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/history/;
-$external_labels{$key} = "$URL/" . q|node26.html|; 
+$external_labels{$key} = "$URL/" . q|node27.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/uncoupled_scripts/;
@@ -47,31 +47,31 @@ $external_labels{$key} = "$URL/" . q|node5.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/pointer_files/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$external_labels{$key} = "$URL/" . q|node26.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/runtypes/;
-$external_labels{$key} = "$URL/" . q|node20.html|; 
+$external_labels{$key} = "$URL/" . q|node21.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:setup_nml/;
 $external_labels{$key} = "$URL/" . q|node8.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_thor75/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/cite_lipshunke04/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:nml_write/;
 $external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_thor75/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/example2_nml/;
-$external_labels{$key} = "$URL/" . q|node18.html|; 
+$external_labels{$key} = "$URL/" . q|node19.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/coupled_scripts/;
@@ -83,7 +83,7 @@ $external_labels{$key} = "$URL/" . q|node9.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/cite_lips01/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/nml_physics/;
@@ -91,19 +91,19 @@ $external_labels{$key} = "$URL/" . q|node12.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/table:history_fields/;
-$external_labels{$key} = "$URL/" . q|node29.html|; 
+$external_labels{$key} = "$URL/" . q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_hunk97/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_cice08/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_Briegleb07/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:ice_prescraero_nml/;
@@ -111,11 +111,15 @@ $external_labels{$key} = "$URL/" . q|node14.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/history_files/;
-$external_labels{$key} = "$URL/" . q|node26.html|; 
+$external_labels{$key} = "$URL/" . q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:domain_nml/;
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_bitz01/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:nml_time/;
@@ -127,7 +131,7 @@ $external_labels{$key} = "$URL/" . q|node13.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/cite_bitz99/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:nml_ocn/;
@@ -135,19 +139,19 @@ $external_labels{$key} = "$URL/" . q|node14.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/change_content/;
-$external_labels{$key} = "$URL/" . q|node29.html|; 
+$external_labels{$key} = "$URL/" . q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/restart_files/;
-$external_labels{$key} = "$URL/" . q|node24.html|; 
+$external_labels{$key} = "$URL/" . q|node25.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_roth75b/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/avg_fields/;
-$external_labels{$key} = "$URL/" . q|node27.html|; 
+$external_labels{$key} = "$URL/" . q|node28.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/example5_nml/;
@@ -163,15 +167,15 @@ $external_labels{$key} = "$URL/" . q|node8.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/example1_nml/;
-$external_labels{$key} = "$URL/" . q|node17.html|; 
+$external_labels{$key} = "$URL/" . q|node18.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:time_grid_hist/;
-$external_labels{$key} = "$URL/" . q|node29.html|; 
+$external_labels{$key} = "$URL/" . q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_hibl79/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 1;
@@ -195,6 +199,10 @@ $noresave{$key} = "$nosave";
 
 $key = q/table:grid_nml/;
 $external_latex_labels{$key} = q|7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:domain_nml/;
+$external_latex_labels{$key} = q|8|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:tracer_nml/;
@@ -246,7 +254,7 @@ $external_latex_labels{$key} = q|1|;
 $noresave{$key} = "$nosave";
 
 $key = q/example2_nml/;
-$external_latex_labels{$key} = q|3.8.2|; 
+$external_latex_labels{$key} = q|3.9.2|; 
 $noresave{$key} = "$nosave";
 
 $key = q/coupled_scripts/;
@@ -262,15 +270,15 @@ $external_latex_labels{$key} = q|2|;
 $noresave{$key} = "$nosave";
 
 $key = q/example1_nml/;
-$external_latex_labels{$key} = q|3.8.1|; 
+$external_latex_labels{$key} = q|3.9.1|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:time_grid_hist/;
-$external_latex_labels{$key} = q|8|; 
+$external_latex_labels{$key} = q|9|; 
 $noresave{$key} = "$nosave";
 
 $key = q/table:history_fields/;
-$external_latex_labels{$key} = q|9|; 
+$external_latex_labels{$key} = q|10|; 
 $noresave{$key} = "$nosave";
 
 1;
